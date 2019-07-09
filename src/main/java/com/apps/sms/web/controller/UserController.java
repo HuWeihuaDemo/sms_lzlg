16fd9669ea1b02f8afbb530b6da43cb210367b46
HuWeihuaDemo <rufenweiweiji@163.com>
2019-07-09 14:15:01 +0800

    【胡伟华】UserController修正

--- src/main/java/com/apps/sms/web/controller/UserController.java
+++ src/main/java/com/apps/sms/web/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
- * java项目入口
+ * java项目入口 
  * @author Administrator
  *
  */
