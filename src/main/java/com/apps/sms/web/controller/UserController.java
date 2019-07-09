5676e06471fbaebc70c51c9d99a2ec5b40f2f6c2
HuWeihuaDemo <rufenweiweiji@163.com>
2019-07-09 10:58:32 +0800

    组长添加用户控制器

--- src/main/java/com/apps/sms/web/controller/UserController.java
+++ src/main/java/com/apps/sms/web/controller/UserController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 用户控制器
+ * @author Administrator
+ *
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
