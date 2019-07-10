b5e6ee94e18fc6dc2052cf3b282059300435a376
骆思璐 <1260198987@qq.com>
2019-07-10 12:02:42 +0800

    [骆思璐]编写了Country的选择程序

--- src/main/java/com/apps/sms/web/controller/CountryController.java
+++ src/main/java/com/apps/sms/web/controller/CountryController.java
@@ -1,5 +1,22 @@
 package com.apps.sms.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.apps.sms.bean.Country;
+import com.apps.sms.service.CountryService;
+
 public class CountryController {
+	
+	@Autowired
+	private CountryService CountryService;
+
+	// http://localhost:8080/school/selectAll
+	@GetMapping("selectAll")
+	public List<Country> selectAll(){
+		return CountryService.selectAll();
+	}
 
 }
