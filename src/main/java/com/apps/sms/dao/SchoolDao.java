1db6d492d22968e972a1bdabe3ae7077498fe070
HuWeihuaDemo <rufenweiweiji@163.com>
2019-07-10 10:37:17 +0800

    【胡伟华】创建config包，更新SchoolDao接口

--- src/main/java/com/apps/sms/dao/SchoolDao.java
+++ src/main/java/com/apps/sms/dao/SchoolDao.java
@@ -1,5 +1,19 @@
 package com.apps.sms.dao;
 
-public class SchoolDao {
+import java.util.List;
 
+import com.apps.sms.bean.School;
+
+public interface SchoolDao {
+	
+	//查询所有
+	List<School> selectAll();
+	
+	//插入
+	void insert(School school);
+	
+	//更新
+	void update(School school);
+	
+	
 }
