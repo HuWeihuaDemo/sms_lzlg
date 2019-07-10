317f2fe1c4ca7832d9cdb72e6cba5dd89ba43180
李金珂 <you@example.com>
2019-07-10 22:59:06 +0800

    [李金珂]完成了UserRole的查询功能

--- src/main/java/com/apps/sms/service/UserRoleService.java
+++ src/main/java/com/apps/sms/service/UserRoleService.java
@@ -1,5 +1,11 @@
 package com.apps.sms.service;
 
+import java.util.List;
+
+import com.apps.sms.bean.UserRole;
+
 public interface UserRoleService {
 
+	List<UserRole> selectAll();
+	void saveOrUpdate(UserRole userrole) throws Exception;
 }
--- src/main/java/com/apps/sms/web/controller/UserRoleController.java
+++ src/main/java/com/apps/sms/web/controller/UserRoleController.java
@@ -1,5 +1,26 @@
 package com.apps.sms.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apps.sms.bean.UserRole;
+import com.apps.sms.service.UserRoleService;
+
+@RestController
+@RequestMapping("/userrole")
 public class UserRoleController {
+	
+	@Autowired
+	private UserRoleService userRoleService;
+
+	// http://localhost:8080/userrole/selectAll
+	@GetMapping("selectAll")
+	public List<UserRole> selectAll(){
+		return userRoleService.selectAll();
+	}
 
 }
