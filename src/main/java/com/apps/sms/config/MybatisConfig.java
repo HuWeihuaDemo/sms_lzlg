cd065ff6ebfff601e765e859d4f4786e58314802
骆思璐 <1260198987@qq.com>
2019-07-11 08:29:06 +0800

    [骆思璐]查询代码二次修正，胡伟华代提交

--- src/main/java/com/apps/sms/config/MybatisConfig.java
+++ src/main/java/com/apps/sms/config/MybatisConfig.java
@@ -6,5 +6,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.apps.sms.dao")
 public class MybatisConfig {
-
+ 
 }
--- src/main/java/com/apps/sms/web/controller/CountryController.java
+++ src/main/java/com/apps/sms/web/controller/CountryController.java
@@ -4,19 +4,23 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.apps.sms.bean.Country;
 import com.apps.sms.service.CountryService;
 
+@RestController
+@RequestMapping("/country")
 public class CountryController {
 	
 	@Autowired
-	private CountryService CountryService;
+	private CountryService countryService;
 
 	// http://localhost:8080/school/selectAll
 	@GetMapping("selectAll")
 	public List<Country> selectAll(){
-		return CountryService.selectAll();
+		return countryService.selectAll();
 	}
 
 }
